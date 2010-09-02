8e0ea674c7943a2656f27dfe8f026350bb3ca635
Finn Lindgren <finn.lindgren@gmail.com>
2010-09-02 17:10:44 +0200

    Added experimental b-spline code

---
+++ rinla/R/inla.version.R
@@ -0,0 +1,9 @@
+`inla.version` = function(details=FALSE, quiet =FALSE) {
+    cat("\n",
+        "This version of R-INLA use the code from inla.googlecode.com",
+        "\n",
+        "No version-number is available.",
+        "\n",
+        "\n")
+    return (invisible())
+}
