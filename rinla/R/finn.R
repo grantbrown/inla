669979b05396bef8110b6fcee5f76102c315d849
Havard Rue <havard.rue@math.ntnu.no>
2011-01-21 08:33:32 +0100

    added unique to avoid potential problem in inla.group for quantile method and low n

---
+++ rinla/R/finn.R
@@ -0,0 +1,39 @@
+## this function interface with mode = finn in the inlaprogram, which
+## computes whatever Finn wants...
+
+`inla.finn` = function(C,
+        reordering = c("auto", "default", "identity", "band", "metis", "genmmd", "amd", "md", "mmd"))
+{
+    reordering = match.arg(reordering)
+
+    if (!is(C, "dgTMatrix"))
+        C = inla.sparse.check(C)
+    
+    if (is(C, "dgTMatrix")) {
+        finn.file = inla.sparse2file(C, c.indexing = TRUE)
+        remove = TRUE
+    } else if (is.character(C)) {
+        finn.file = C
+        remove = FALSE
+    } else {
+        stop("This should not happen.")
+    }
+        
+    if (inla.os("linux") || inla.os("mac")) {
+        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m finn",
+                "-r", reordering, finn.file), intern=TRUE)
+    } else if(inla.os("windows")) {
+        s = system(paste(shQuote(inla.getOption("inla.call")), "-s -m finn",
+                "-r", reordering, finn.file), intern=TRUE)
+    } else {
+        stop("\n\tNot supported architecture.")
+    }
+
+    if (remove) {
+        unlink(finn.file)
+    }
+
+    s = as.numeric(s)
+    n = length(s) %/% 2L
+    return ( list(sample = s[1L:n], reordering = s[n + (1L:n)] + 1L))
+}
