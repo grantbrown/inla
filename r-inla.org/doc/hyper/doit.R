dd05d9515529a26241e5dcfd3892ca6a902a9ddf
Havard Rue <havard.rue@math.ntnu.no>
2011-11-21 20:37:52 +0100

    Another try for the Win-problem in R-2.14 with option verbose=FALSE.

---
+++ r-inla.org/doc/hyper/doit.R
@@ -0,0 +1,15 @@
+options(keep.source=TRUE)
+source("../../../rinla/R/models.R")
+source("../../../rinla/R/models-generate.R")
+source("../../../rinla/R/utils.R")
+source("../../../rinla/R/inla.version.R")
+m = inla.models()
+for(nm in names(m)) {
+    if (!file.exists(nm))
+        dir.create(nm)
+    for (mod  in names(m[[nm]])) {
+        fnm = paste(nm, "/", mod, ".tex",  sep="")
+        cat("Generate file", fnm, "\n")
+        inla.models.generate.tex(m[[nm]][[mod]], fnm)
+    }
+}
