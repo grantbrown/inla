84962f81698f3aa1d1bc08f73a85c4c19de9d278
Finn Lindgren <finn.lindgren@gmail.com>
2013-02-23 16:36:10 +0000

    Bug fixes for boundary/interior segment handling and inla.sp2segment conversion from sp-package objects (Fixes issue #12).

---
+++ rinla/R/posterior.sample.R
@@ -0,0 +1,119 @@
+##! \name{inla.sample}
+##! \alias{inla.posterior.sample}
+##! \alias{posterior.sample}
+##! 
+##! \title{Generate samples from an approximated posterior of a fitted model}
+##! 
+##! \description{This function generate samples from an approximated posterior of a fitted model,  ie an inla-object}
+##! \usage{
+##!     inla.posterior.sample(n = 1L, result, hyper.user.scale = TRUE)
+##! }
+##! 
+##! \arguments{
+##!   \item{n}{Number of samples.}
+##!   \item{result}{The inla-object, ie the output from an \code{inla}-call. The \code{inla}-object must be created with
+##!                 \code{control.compute=list(config=TRUE)}.}
+##!   \item{hyper.user.scale}{Use values of hyperparameters in the user scale, not in the internal representation}
+##!}
+##!\value{
+##!  A list of the samples,  where each sample is a list with names \code{hyperpar} and \code{latent},  and with
+##!  their marginal densities in \code{logdens$hyperpar} and \code{logdens$latent}
+##!  and the joint density is in \code{logdens$joint}.
+##!  THIS IS AN EXPERIMENTAL FUNCTION AND CHANGES MAY APPEAR AT ANY TIME!
+##!}
+##!\author{Havard Rue \email{hrue@math.ntnu.no}}
+##! 
+##!\examples{
+##!  r = inla(y ~ 1 ,data = data.frame(y=rnorm(1)), control.compute = list(config=TRUE))
+##!  samples = inla.posterior.sample(2,r)
+##!}
+
+
+`inla.posterior.sample` = function(n = 1, result, hyper.user.scale = TRUE)
+{
+    warning("THIS FUNCTION IS EXPERIMENTAL!!!")
+    
+    stopifnot(!missing(result) && any(class(result) == "inla"))
+    if (is.null(result$misc$configs)) {
+        stop("You need an inla-object computed with option 'control.compute=list(config = TRUE)'.")
+    }
+    n = as.integer(n)
+    stopifnot(is.integer(n) && n > 0L)
+    
+    ## first sample the theta(-index)
+    cs = result$misc$configs
+    ld = numeric(cs$nconfig)
+    for (i in 1:cs$nconfig) {
+        ld[i] = cs$config[[i]]$log.posterior
+    }
+    p = exp(ld - max(ld))
+    idx = sample(1:cs$nconfig, n, prob = p, replace = TRUE)
+    idx = sort(idx)
+    n.idx = numeric(cs$nconfig)
+    n.idx[] = 0
+    for(i in 1:cs$nconfig) {
+        n.idx[i] = sum(idx == i)
+    }
+
+    all.samples = list(list())
+    all.samples[[n]] = NA
+    i.sample = 1L
+    for(k in 1:cs$nconfig) {
+        if (n.idx[k] > 0) {
+            ## then the latent field
+            xx = inla.qsample(n=n.idx[k], Q=cs$config[[k]]$Q, mu = cs$config[[k]]$mean, constr = cs$constr, logdens = TRUE)
+            nm = c()
+            ld.theta = cs$max.log.posterior + cs$config[[k]]$log.posterior
+            for(j in 1:length(cs$contents$tag)) {
+                ii = seq(cs$contents$start[j], length = cs$contents$length[j])
+                nm = c(nm,
+                        paste(cs$contents$tag[j],
+                              ".",
+                              inla.ifelse(cs$contents$length[j] == 1L, 1, inla.num(1:cs$contents$length[j])), 
+                              sep=""))
+            }
+            
+            theta = cs$config[[k]]$theta
+            log.J = 0.0
+            if (!is.null(theta) && hyper.user.scale) {
+                for(j in 1:length(theta)) {
+                    theta[j] = do.call(result$misc$from.theta[[j]], args = list(theta[j]))
+                }
+                names(theta) = paste(names(theta), "-- in user scale")
+                
+                h = .Machine$double.eps^0.25
+                for(i in 1:length(theta)) {
+                    theta.1 = do.call(result$misc$from.theta[[i]], args = list(cs$config[[k]]$theta[i] - h))
+                    theta.2 = do.call(result$misc$from.theta[[i]], args = list(cs$config[[k]]$theta[i] + h))
+                    log.J = log.J - log(abs((theta.2 - theta.1)/(2.0*h))) ## Yes, it's a minus...
+                }
+            }
+            
+            for(i in 1:n.idx[k]) {
+                if (is.null(theta)) {
+                    a.sample = list(
+                            hyperpar = NULL,
+                            latent = xx$sample[ , i, drop=FALSE],
+                            logdens = list(
+                                    hyperpar = NULL,
+                                    latent = as.numeric(xx$logdens[i]),
+                                    joint = as.numeric(xx$logdens[i])))
+                } else {
+                    ld.h = as.numeric(ld.theta - result$mlik[1, 1] + log.J)
+                    a.sample = list(
+                            hyperpar = theta,
+                            latent = xx$sample[ , i, drop=FALSE],
+                            logdens = list(
+                                    hyperpar = ld.h,
+                                    latent = as.numeric(xx$logdens[i]),
+                                    joint = as.numeric(ld.h + xx$logdens[i])))
+                }
+                rownames(a.sample$latent) = nm
+                all.samples[[i.sample]] = a.sample
+                i.sample = i.sample + 1L
+            }    
+        }
+    }
+    
+    return (all.samples)
+}
