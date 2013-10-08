34bd63fbc002028b26d418c8a79ea51701297e8e
eliaskrainski <eliaskrainski@localhost>
2013-10-08 17:06:20 +0200

    spde tutorial improvements on two likelihood examples

---
+++ r-inla.org/doc/hyper/doit.R
@@ -0,0 +1,16 @@
+options(keep.source=TRUE)
+source("../../../rinla/R/inlaEnv.R")
+source("../../../rinla/R/models.R")
+source("../../../rinla/R/models-generate.R")
+source("../../../rinla/R/utils.R")
+source("../../../rinla/R/version.R")
+m = inla.models()
+for(nm in names(m)) {
+    if (!file.exists(nm))
+        dir.create(nm)
+    for (mod  in names(m[[nm]])) {
+        fnm = paste(nm, "/", mod, ".tex",  sep="")
+        cat("Generate file", fnm, "\n")
+        inla.models.generate.tex(m[[nm]][[mod]], fnm)
+    }
+}
