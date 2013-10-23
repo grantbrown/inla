bb86ec764ba9eb2725f540df0023c58a9544fe34
Finn Lindgren <finn.lindgren@gmail.com>
2013-10-23 20:44:19 +0100

    Added reordering="metis" to the Cameletti2012-case-study.

---
+++ rinla/R/link-functions.R
@@ -0,0 +1,139 @@
+## Export: inla.link.log inla.link.invlog inla.link.logit inla.link.invlogit inla.link.probit
+## Export: inla.link.invprobit inla.link.cloglog inla.link.invcloglog inla.link.tan inla.link.invtan
+## Export: inla.link.identity inla.link.invidentity inla.link.invalid
+
+##! \name{link}
+##! \alias{link}
+##! \alias{inla.link}
+##! \alias{inla.link.log}
+##! \alias{inla.link.invlog}
+##! \alias{inla.link.logit}
+##! \alias{inla.link.invlogit}
+##! \alias{inla.link.probit}
+##! \alias{inla.link.invprobit}
+##! \alias{inla.link.cloglog}
+##! \alias{inla.link.invcloglog}
+##! \alias{inla.link.tan}
+##! \alias{inla.link.invtan}
+##! \alias{inla.link.identity}
+##! \alias{inla.link.invidentity}
+##! \alias{inla.link.invalid}
+##! \alias{inla.link.invinvalid}
+##! 
+##! \title{Link functions in INLA}
+##! 
+##! \description{Define link-functions and its inverse}
+##!
+##! \usage{
+##! inla.link.log(x, inverse=FALSE)
+##! inla.link.invlog(x, inverse=FALSE)
+##! inla.link.logit(x, inverse=FALSE)
+##! inla.link.invlogit(x, inverse=FALSE)
+##! inla.link.probit(x, inverse=FALSE)
+##! inla.link.invprobit(x, inverse=FALSE)
+##! inla.link.cloglog(x, inverse=FALSE)
+##! inla.link.invcloglog(x, inverse=FALSE)
+##! inla.link.tan(x, inverse=FALSE)
+##! inla.link.invtan(x, inverse=FALSE)
+##! inla.link.identity(x, inverse=FALSE)
+##! inla.link.invidentity(x, inverse=FALSE)
+##! inla.link.invalid(x, inverse=FALSE)
+##! inla.link.invinvalid(x, inverse=FALSE)
+##! }
+##! 
+##! \arguments{
+##!     \item{x}{The argument. A numeric vector.}
+##!     \item{inverse}{Logical. Use the link (\code{inverse=FALSE})
+##!                    or its inverse (\code{inverse=TRUE})}
+##!}
+##! 
+##! \value{Return the values of the link-function or its inverse.}
+##! \note{The \code{inv}-functions are redundant,  as 
+##!       \code{inla.link.invlog(x) = inla.link.log(x, inverse=TRUE)}
+##!       and so on,  but they are simpler to use a arguments
+##!       to other functions.}
+##! \author{Havard Rue \email{hrue@math.ntnu.no}}
+
+`inla.link.log` = function(x, inverse = FALSE)
+{
+    if (!inverse) {
+        return (log(x))
+    } else {
+        return (exp(x))
+    }
+}
+`inla.link.invlog` = function(x, inverse = FALSE)
+{
+    return (inla.link.log(x, inverse = !inverse))
+}
+
+`inla.link.logit` = function(x, inverse = FALSE)
+{
+    if (!inverse) {
+        return (log(x/(1.0-x)))
+    } else {
+        return (1.0/(1.0+exp(-x)))
+    }
+}
+`inla.link.invlogit` = function(x, inverse = FALSE)
+{
+    return (inla.link.logit(x, inverse = !inverse))
+}
+
+`inla.link.probit` = function(x, inverse = FALSE)
+{
+    if (!inverse) {
+        return (qnorm(x))
+    } else {
+        return (pnorm(x))
+    }
+}
+`inla.link.invprobit` = function(x, inverse = FALSE)
+{
+    return (inla.link.probit(x, inverse = !inverse))
+}
+
+`inla.link.cloglog` = function(x, inverse = FALSE)
+{
+    if (!inverse) {
+        return (-log(-log(x)))
+    } else {
+        return (exp(-exp(-x)))
+    }
+}
+`inla.link.invcloglog` = function(x, inverse = FALSE)
+{
+    return (inla.link.cloglog(x, inverse = !inverse))
+}
+
+`inla.link.tan` = function(x, inverse = FALSE)
+{
+    if (!inverse) {
+        return (tan(x/2.0))
+    } else {
+        return (2.0*atan(x))
+    }
+}
+`inla.link.invtan` = function(x, inverse = FALSE)
+{
+    return (inla.link.tan(x, inverse = !inverse))
+}
+`inla.link.identity` = function(x, inverse = FALSE)
+{
+    return (x)
+}
+`inla.link.invidentity` = function(x, inverse = FALSE)
+{
+    return (x)
+}
+
+
+## These are the invalid ones
+`inla.link.invalid` = function(x, inverse = FALSE)
+{
+    stop("The invalid link-function is used.")
+}
+`inla.link.invinvalid` = function(x, inverse = FALSE)
+{
+    stop("The invinvalid link-function is used.")
+}
---
+++ rinla/R/sm.R
@@ -0,0 +1,161 @@
+## Export: inla.as.sparse inla.as.dgTMatrix
+
+##!\name{inla.as.sparse}
+##!\alias{inla.as.sparse}
+##!\alias{inla.as.dgTMatrix}
+##!
+##!\title{Convert a matrix or sparse matrix into the sparse formate used by INLA}
+##!
+##!\description{Convert a matrix or sparse matrix into the sparse format used by INLA (dgTMatrix)}
+##!
+##!\usage{
+##! inla.as.sparse(...)
+##! inla.as.dgTMatrix(A, unique = TRUE)
+##!}
+##!
+##!\arguments{
+##!  \item{...}{The arguments. The matrix or sparse matrix,  and the additonal arguments}
+##!  \item{A}{The matrix}
+##!  \item{unique}{If the internal representation needs to be unique or can have duplicated entries.
+##!                Do not use this option unless you know what you are doing.}
+##!}
+##!
+##!\value{%%
+##!  \code{inla.as.sparse} and \code{inla.as.dgTMatrix} is the same function.
+##!  The returned value is a sparse matrix in the sparse-format used by INLA
+##!}
+##!%%
+##!
+##!\author{Havard Rue \email{hrue@math.ntnu.no}}
+##!
+##!\examples{
+##! A = matrix(1:9, 3, 3)
+##! inla.as.sparse(A)
+##!}
+
+`inla.as.sparse` = function(...)
+{
+    return (inla.as.dgTMatrix(...))
+}
+
+
+`inla.idx` = function(idx, n = max(idx), group = rep(1, n), ngroup = max(group),
+        replicate = rep(1, n), nrep = max(replicate))
+{
+    ## this function might be useful to convert from (idx, group, rep)
+    ## to idx, in the same way as done internally in inla.R
+
+    stopifnot(n >= 1)
+    stopifnot(ngroup >= 1)
+    stopifnot(nrep >= 1)
+    stopifnot(all(group >= 1))
+    stopifnot(all(replicate >= 1))
+    stopifnot(all(idx >= 1))
+    stopifnot(all(idx <= n))
+    stopifnot(ngroup >= max(group))
+    stopifnot(nrep >= max(replicate))
+
+    return (idx + (group-1)*n + (replicate-1)*n*ngroup)
+}
+
+
+### Some utilities for sparse matrices using the `Matrix' library
+
+`inla.sparse.dim` = function(A) {
+    ## return the dimension of the matrix A
+    A = inla.sparse.check(A, must.be.squared = FALSE)
+
+    if (is.character(A)) {
+        Am = read.table(A, col.names = c("i", "j", "Aij"))
+        if (min(Am$i) == 0 || min(Am$j) == 0) {
+            cindex = 1
+        } else {
+            cindex = 0
+        }
+        return (c(max(Am$i) + cindex, max(Am$j) + cindex))
+    } else {
+        return (dim(A))
+    }
+}
+
+
+`inla.sparse.check` = function(A, must.be.squared = TRUE)
+{
+    ## just check if matrix A exists and return either a filename or a
+    ## dgTMatrix.
+    if (is.character(A)) {
+        if (!file.exists(A)) {
+            stop(paste("File not found:", A))
+        }
+        return (A)
+    }
+
+    if (is.list(A))
+        stop("Define matrix using Matrix::sparseMatrix() instead!!! The list(i=, j=, values=)-format is obsolete!")
+
+    if (must.be.squared) {
+        if (dim(A)[1] != dim(A)[2]) {
+            stop(paste(c("Matrix is not a square matrix:", dim(A)[1], "x", dim(A)[2])))
+        }
+    }
+
+    return (inla.as.dgTMatrix(A))
+}
+
+`inla.as.dgTMatrix` = function(A, unique = TRUE)
+{
+    ## convert into dgTMatrix format of Matrix. Argument A is any
+    ## matrix.  make sure the representation is unique if the UNIQUE
+    ## flag it TRUE. (ie no double triplets etc)
+
+    if (unique) {
+        ## convert through the 'dgCMatrix'-class to make it unique;
+        ## (there is no method or default for coercing "dtTMatrix" to
+        ## "dgCMatrix"). Yes: its 'CsparseMatrix'.
+        return (as(as(as(A, "CsparseMatrix"), "dgCMatrix"), "dgTMatrix"))
+    } else {
+        if (is(A, "dgTMatrix")) {
+            return (A)
+        } else {
+            ## Convert via virtual class TsparseMatrix;
+            ## this allows more general conversions than direct conversion.
+            return (as(as(A, "TsparseMatrix"), "dgTMatrix"))
+        }
+    }
+}
+
+`inla.sparse.get` = function(A, row, col)
+{
+    ## extract a list of the a specific row or col of a dgTMatrix
+    ## A. the list returned is of type list(i=..., j=..., values=...)
+
+    if (missing(row) && missing(col))
+        return (list(i=numeric(0), j=numeric(0), values=numeric(0)))
+
+    if (!missing(row) && !missing(col))
+        stop("Only one of 'row' and 'col' can be specified.")
+
+    ## need this particular format. I think this is faster for
+    ## repeated use, as we do not need to duplicate the A matrix in
+    ## memory. but perhaps I'm wrong...
+    if (!is(A, "dgTMatrix")) {
+        ## This can be slow, so its better to stop and say that the
+        ## matrix has to be converted upfront.
+        stop("Matrix is not of type 'dgTMatrix'; please convert it with inla.as.dgTMatrix().")
+        A = inla.as.dgTMatrix(A)
+    }
+
+    if (!missing(row)) {
+        stopifnot(row >=1 && row <= A@Dim[1])
+        stopifnot(length(row) == 1)
+
+        idx = which(A@i == row-1)
+        return (list(i = row, j = A@j[idx]+1, values = A@x[idx]))
+    } else {
+        stopifnot(col >=1 && col <= A@Dim[2])
+        stopifnot(length(col) == 1)
+
+        idx = which(A@j == col-1)
+        return (list(i = A@i[idx]+1, j = col, values = A@x[idx]))
+    }
+}
