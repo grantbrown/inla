cbc15efd21764bab9889fb7c35160776d9574dfa
Finn Lindgren <finn.lindgren@gmail.com>
2014-09-01 11:21:19 +0200

    Reactivate experimental spe3 interface

---
+++ Makefile
@@ -0,0 +1,48 @@
+## keep track of updates needed to be done
+
+all:; \
+	## update the TAGS-file and the man-files that are generated
+	## automatically 
+	-make -C rinla/R 
+	## update the doc extracted from the R-files
+	-make -C r-inla.org/doc
+	## update the TAGS-file
+	-make -C gmrflib TAGS
+	## update the TAGS-file
+	-make -C inlaprog src/TAGS
+	##
+	-make doc-links
+
+
+## The following finds all .tex files in
+##   r-inla.org/doc/{prior,latent,likelihood}
+## and adds links in rinla/inst/doc/... to the corresponding .pdf files
+## In the past it redirected to
+##   ../../../../../../google-code/inla/
+## Now it redirects to
+##   ../../../../
+## which keeps it inside the mercurial clone.
+doc-links:
+	@echo "Building documentation links from r-inla.org to rinla."
+	@for dir in prior latent likelihood link; do \
+	  find "r-inla.org/doc/$$dir" -name \*.tex | \
+		sed "s!r-inla.org/doc/\(.*\)\.tex!ln -sf ../../../../r-inla.org/doc/\1.pdf rinla/inst/doc/\1.pdf!" | sh -e ;\
+	  hg status r-inla.org/doc/$$dir/*.tex ;\
+	done
+	hg status rinla/inst/doc/
+	@echo "Documentation link building finished."
+
+## Build a INLA-package without the binaries
+INLA-package:
+	utils/build.package
+
+## Check the latest built package from INLA-package:
+R = R
+## R = ~/R-dev/bin/R
+FILENAME := $(shell ls -rt ~/tmp/.BuildINLA/INLA_*.tgz | grep -v _R_ | tail -1 )
+INLA-package-check:
+	@echo Checking $(FILENAME)
+	$(R) --vanilla CMD check --no-examples $(FILENAME)
+
+##
+.PHONY: doc-links INLA-package all INLA-package-check
---
+++ fmesher/src/basis.cc
@@ -0,0 +1,157 @@
+#include <cstddef>
+#include <cstring>
+#include <set>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include "gsl/gsl_sf_legendre.h"
+
+#include "vector.hh"
+#include "ioutils.hh"
+
+#define WHEREAMI __FILE__ << "(" << __LINE__ << ")\t"
+
+#ifdef DEBUG
+//#define _LOG(msg) std::cout << WHEREAMI << msg;
+#define _LOG(msg)
+#else
+#define _LOG(msg)
+#endif
+
+#define M_2_SQRT_PI 3.5449077018110320546
+
+
+using std::ios;
+using std::cout;
+using std::cin;
+using std::endl;
+
+namespace fmesh {
+
+  int sph_basis_n(int kmax, bool rot_sym)
+  {
+    if (kmax >= 0) {
+      if (rot_sym)
+	return (kmax + 1);
+      else
+	return (kmax + 1) * (kmax + 1);
+    } else {
+      return 0;
+    }
+  }
+
+  Matrix<double> spherical_harmonics(const Matrix3<double>& S,
+				     size_t max_order,
+				     bool rotationally_symmetric)
+  {
+    Matrix<double> sph(sph_basis_n(max_order,rotationally_symmetric));
+    size_t i, k, m;
+    double GSL_res_array[max_order+1];
+
+    if (rotationally_symmetric) {    
+      for (i=0; i<S.rows(); i++) {
+	gsl_sf_legendre_sphPlm_array(max_order, 0, S[i][2], GSL_res_array);
+	for (k = 0; k <= max_order; k++) {
+	  sph(i,k) = M_2_SQRT_PI * GSL_res_array[k];
+	}
+      }
+    } else {
+        double phi, scaling_sin, scaling_cos;
+
+	size_t Idxs2[max_order + 1];
+	// 0, 2, 6, 12, 20, 30, 42, 56, 72, 90, 110, 132, 156, 182, 210, 240
+	for (k = 0; k <= max_order; k++) {
+	  Idxs2[k] = k*(k+1);
+	}
+
+	for (i=0; i<S.rows(); i++) {
+	  phi = atan2(S[i][1], S[i][0]);
+	  
+	  gsl_sf_legendre_sphPlm_array(max_order, 0, S[i][2], GSL_res_array);
+	  for (k = 0; k <= max_order; k++) {
+	    sph(i,Idxs2[k]) = M_2_SQRT_PI * GSL_res_array[k];
+	  }
+	  for (m = 1; m <= max_order; m++) {
+	    scaling_sin = M_2_SQRT_PI * M_SQRT2 * sin(-(m * phi));
+	    scaling_cos = M_2_SQRT_PI * M_SQRT2 * cos(m * phi);
+	    gsl_sf_legendre_sphPlm_array(max_order, m, S[i][2], GSL_res_array);
+	    for (k = m; k <= max_order; k++) {
+	      sph(i,Idxs2[k] - m) = scaling_sin * GSL_res_array[k - m];
+	      sph(i,Idxs2[k] + m) = scaling_cos * GSL_res_array[k - m];
+	    }
+	  }
+	}
+
+   }
+
+    return sph;
+  }
+
+
+  Matrix<double> spherical_bsplines(const Matrix3<double>& S,
+				    size_t n_basis,
+				    size_t degree,
+				    bool uniform_knot_angle_spacing)
+  {
+    Matrix<double> basis(n_basis);
+    double knots[n_basis+degree+1];
+    double s,s1,s2;
+    Matrix<double> control[n_basis];
+    Matrix<double> control_work[degree+1];
+    size_t interval;
+
+    for (size_t i=0; i<=degree; i++) {
+      knots[i] = -1.0;
+    }
+    for (size_t i=degree+1; i<n_basis; i++) {
+      knots[i] = (double(i-degree)/double(n_basis-degree))*2.0-1.0;
+      if (uniform_knot_angle_spacing) {
+	knots[i] = sin(knots[i]*M_PI/2.0);
+      }
+    }
+    for (size_t i=n_basis; i<=n_basis+degree; i++) {
+      knots[i] = 1.0;
+    }
+
+    for (size_t i=0; i<n_basis; i++) {
+      control[i] = Matrix<double>(n_basis);
+      control[i](0,i) = 1.0;
+    }
+
+    _LOG("degree\t" << degree << endl);
+    _LOG("n_basis\t" << n_basis << endl);
+    _LOG("n_basis+degree+1\t" << n_basis+degree+1 << endl);
+
+    for (size_t coord_idx=0; coord_idx<S.rows(); coord_idx++) {
+      s = S[coord_idx][2];
+
+      _LOG("step 1, coord_idx\t" << coord_idx << endl);
+      interval = degree;
+      while ((interval+1<n_basis) & (s>=knots[interval+1]))
+	interval++;
+      
+      _LOG("step 2" << endl);
+      for (size_t i=0; i<=degree; i++)
+	control_work[i] = control[i+interval-degree];
+      
+      _LOG("step 3" << endl);
+      for (size_t k=1; k<=degree; k++)
+	for (size_t i=degree; i>=k; i--) {
+	  s1 = (knots[i+interval-k+1] - s)/(knots[i+interval-k+1]-knots[i+interval-degree]);
+	  s2 = 1.0-s1;
+
+	  for (size_t j=0; j<n_basis; j++)
+	    control_work[i](0,j) = (s1 * control_work[i-1](0,j) +
+				    s2 * control_work[i](0,j));
+	}
+      
+      for (size_t j=0; j<n_basis; j++)
+	basis(coord_idx,j) = control_work[degree](0,j);
+    }
+
+    return basis;
+}
+
+
+
+} /* namespace fmesh */
